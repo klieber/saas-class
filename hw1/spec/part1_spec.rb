9d13755d559f9b58fe4298922b25a773a08fc0c0
Kyle Lieber <klieberuni@gmail.com>
2012-02-29 15:31:12 -0800

    fixed format

--- hw1/spec/part1_spec.rb
+++ hw1/spec/part1_spec.rb
@@ -1,25 +1,21 @@
 $LOAD_PATH << '../lib'
 require 'part1'
 
-describe Part1, "#palindrome?" do
+describe "#palindrome?" do
   it "returns true for 'A man, a plan, a canal -- Panama'" do
-    part1 = Part1.new
-    part1.palindrome?("A man, a plan, a canal -- Panama").should eq(true)
+    palindrome?("A man, a plan, a canal -- Panama").should eq(true)
   end
   it "returns true for 'Madam, I'm Adam!'" do
-    part1 = Part1.new
-    part1.palindrome?("Madam, I'm Adam!").should eq(true)
+    palindrome?("Madam, I'm Adam!").should eq(true)
   end
   it "returns false for 'Abracadabra'" do
-    part1 = Part1.new
-    part1.palindrome?("Abracadabra").should eq(false)
+    palindrome?("Abracadabra").should eq(false)
   end
 end
 
-describe Part1, "#count_words" do
+describe "#count_words" do
   it "returns {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1 } for 'A man, a plan, a canal -- Panama'" do
-    part1 = Part1.new
-    words = part1.count_words("A man, a plan, a canal -- Panama")
+    words = count_words("A man, a plan, a canal -- Panama")
     words.should include(:a => 3)
     words.should include(:man => 1)
     words.should include(:canal => 1)
@@ -28,8 +24,7 @@ describe Part1, "#count_words" do
   end
   
   it "returns {'doo' => 3, 'bee' => 2} for 'Doo bee doo bee doo'" do
-    part1 = Part1.new
-    words = part1.count_words("Doo bee doo bee doo")
+    words = count_words("Doo bee doo bee doo")
     words.should include(:doo => 3)
     words.should include(:bee => 2)
   end
