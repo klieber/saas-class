da34c44497a573455fee273360ff7db4ab83c615
Kyle Lieber <klieberuni@gmail.com>
2012-02-28 16:34:41 -0800

    implemented

---
+++ hw1/lib/part1.rb
@@ -0,0 +1,14 @@
+class Part1
+  def palindrome?(string)
+    a = string.gsub(/[^\w*]*/,'').downcase
+    return a == a.reverse 
+  end
+
+  def count_words(string)
+    h = Hash.new(0)
+    string.downcase.scan(/\w+/).each do |word|
+      h[word.to_sym] += 1
+    end
+    return h
+  end
+end
