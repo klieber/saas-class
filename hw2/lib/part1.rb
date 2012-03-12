b5a008612a2180926fd1b9fc4f956126a2720333
Kyle Lieber <klieberuni@gmail.com>
2012-03-11 18:46:27 -0700

    fixes

--- hw2/lib/part1.rb
+++ hw2/lib/part1.rb
@@ -1,27 +1,37 @@
-# metaprogramming to the rescue!
+#c metaprogramming to the rescue!
 
 class Numeric
+  attr_accessor :currency
+  
   @@currencies = {
     'dollar' => 1,
     'yen' => 0.013,
     'euro' => 1.292,
     'rupee' => 0.019
   }
-
+ 
   def in(currency) 
-    eval %Q[self.#{currency}]
+    singular_currency = currency.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self / @@currencies[singular_currency]
+    end 
   end 
 
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
-      self * @@currencies[singular_currency]
+      result=self * @@currencies[singular_currency]
+      result.currency = singular_currency
+      result
     else
       super
     end
   end
 end
 
+puts 1.dollar.in(:euros)
+puts 5.yen.in(:euros)
+puts 5.rupees.in(:yen)
 puts 5.euros
 puts 5.yen
 puts 5.rupee
@@ -29,6 +39,8 @@ puts 5.dollars
 puts 1.dollar.in(:euros)
 puts 1.dollar.in(:rupees)
 puts 10.rupees.in(:euro)
+puts 5.rupees.in(:yen).between?(7.2, 7.4)
+puts 5.rupees.in(:yen)
 
 class String 
   def palindrome?
@@ -42,9 +54,12 @@ puts "woow".palindrome?
 
 module Enumerable
   def palindrome?
-    self == self.reverse
+    a = self.collect { |obj| obj }
+    a == a.reverse
+    #self.respond_to?(:reverse) && self == self.reverse
   end
 end
 
 puts [1,2,3,2,1].palindrome?
 puts [2,3,2,1].palindrome?
+puts ({'a' => 1, 'b' => 2}).palindrome?
